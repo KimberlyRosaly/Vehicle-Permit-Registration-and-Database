7b834cd57e6cf387a32a1bec993012254cee7f4d
VQuine <01010110.v@gmail.com>
2020-07-28 19:46:50 -0400

    scaffold PERMIT with Corneal

---
+++ app/controllers/permits_controller.rb
@@ -0,0 +1,37 @@
+class PermitsController < ApplicationController
+
+  # GET: /permits
+  get "/permits" do
+    erb :"/permits/index.html"
+  end
+
+  # GET: /permits/new
+  get "/permits/new" do
+    erb :"/permits/new.html"
+  end
+
+  # POST: /permits
+  post "/permits" do
+    redirect "/permits"
+  end
+
+  # GET: /permits/5
+  get "/permits/:id" do
+    erb :"/permits/show.html"
+  end
+
+  # GET: /permits/5/edit
+  get "/permits/:id/edit" do
+    erb :"/permits/edit.html"
+  end
+
+  # PATCH: /permits/5
+  patch "/permits/:id" do
+    redirect "/permits/:id"
+  end
+
+  # DELETE: /permits/5/delete
+  delete "/permits/:id/delete" do
+    redirect "/permits"
+  end
+end
