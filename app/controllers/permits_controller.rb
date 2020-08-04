c4b02b5ce94cacd8d44812239899323c219a2226
VQuine <01010110.v@gmail.com>
2020-08-03 22:48:31 -0400

    flesh out permit's edit page, delete button

--- app/controllers/permits_controller.rb
+++ app/controllers/permits_controller.rb
@@ -31,12 +31,29 @@ class PermitsController < ApplicationController
 
   # GET: /permits/5/edit
   get "/permits/:id/edit" do
+    @permit = Permit.find(params[:id])
+    @apartment = Apartment.find(@permit.apartment_id)
     erb :"/permits/edit.html"
   end
 
   # PATCH: /permits/5
   patch "/permits/:id" do
-    redirect "/permits/:id"
+    @apartment = Apartment.find_by(building: params[:building], unit: params[:unit])
+    if @apartment == nil
+      @apartment = Apartment.create(building: params[:building].upcase, unit: params[:unit].upcase, number: params[:building].upcase + params[:unit].upcase)
+    end
+    @permit = Permit.find(params[:id])
+    @permit.update(
+      apartment_id: @apartment.id, 
+      number: params[:number], 
+      tenant_name: params[:tenant_name], 
+      contact_number: params[:contact_number], 
+      vehicle_plate: params[:vehicle_plate], 
+      vehicle_color: params[:vehicle_color], 
+      vehicle_year: params[:vehicle_year], 
+      vehicle_make_model: params[:vehicle_make_model] 
+    )
+    redirect "/permits/#{@permit.id}"
   end
 
   # DELETE: /permits/5/delete
